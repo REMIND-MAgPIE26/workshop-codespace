51af9f2e90d9e4f62b0dcae3a164a22692022e3b
Patrick R <codeZeilen@users.noreply.github.com>
2026-06-02 12:29:32 +0000

    Adds installation of tinytex

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -8,6 +8,7 @@ if [ -n "$GAMS_LICENSE" ]; then
   echo "$GAMS_LICENSE" > /opt/gams/gamslice.txt
 fi
 
+
 #
 # Clone and setup folders
 #
@@ -18,12 +19,12 @@ rm .codeCheck
 wget https://raw.githubusercontent.com/magpiemodel/magpie/refs/heads/develop/.codeCheck
 cd ..
 
-
 cd /workspaces/magpie
 Rscript -e '2 + 2; "dummy evaluation to trigger renv install"'
 
 cd /workspaces/mrtutorial
 Rscript -e 'pak::pak(".")'
+
 
 #
 # Setup madrat folder
@@ -37,3 +38,15 @@ echo "" >> ~/.bashrc
 # Also set the mainfolder for the current session already,
 # as the .bashrc is only loaded later.
 MADRAT_MAINFOLDER='/workspaces/madrat-main-folder'
+
+
+#
+# Install TinyTeX
+#
+Rscript -e 'install.packages("tinytex"); tinytex::install_tinytex(force = T)'
+
+echo "" >> ~/.bashrc
+echo "export PATH=$PATH:~/bin" >> ~/.bashrc
+echo "" >> ~/.bashrc
+
+PATH=$PATH:~/bin
